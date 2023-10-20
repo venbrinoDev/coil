9d273f7b2c5d8df6a9ac3a0835d1192212293266
Jeremiah Ogbomo <jeremiahogbomo@gmail.com>
2023-10-20 14:20:03 +0200

    A

--- coil/lib/coil.dart
+++ coil/lib/coil.dart
@@ -1,1 +1,54 @@
-class Coil<T> {}
+abstract class Ref {
+  T use<T>(Coil<T> coil);
+
+  void mutate<T>(Coil<T> coil, T Function(T) updater);
+
+  void invalidate<T>(Coil<T> coil);
+}
+
+typedef RefFactory<T> = T Function(Ref);
+
+abstract class Coil<T> {
+  RefFactory<T> get _factory;
+
+  late final int _key = identityHashCode(this);
+}
+
+class Scope implements Ref {
+  Scope() : _bucket = {};
+
+  final Map<int, Object> _bucket;
+
+  @override
+  T use<T>(Coil<T> coil) {
+    switch (_bucket[coil._key]) {
+      case final T state?:
+        return state;
+      case _:
+        final state = coil._factory(this);
+        _bucket[coil._key] = state as Object;
+        return state;
+    }
+  }
+
+  @override
+  void mutate<T>(Coil<T> coil, T Function(T p1) updater) {
+    // TODO: implement mutate
+  }
+
+  @override
+  void invalidate<T>(Coil<T> coil) {
+    // TODO: implement invalidate
+  }
+}
+
+Coil<T> coil<T>(RefFactory<T> factory) {
+  return _ValueCoil(factory);
+}
+
+class _ValueCoil<T> extends Coil<T> {
+  _ValueCoil(this._factory);
+
+  @override
+  final RefFactory<T> _factory;
+}
--- coil_example/bin/coil_example.dart
+++ coil_example/bin/coil_example.dart
@@ -1,3 +1,13 @@
+import 'package:coil/coil.dart';
+
+final firstname = coil((_) => 'First');
+final lastname = coil((_) => 'Last');
+final age = coil((_) => 0);
+final fullname = coil((ref) => '${ref.use(firstname)} ${ref.use(lastname)}');
+final result = coil((ref) => '${ref.use(fullname)} (${ref.use(age)})');
+
 void main() {
-  print('Hello world');
+  final scope = Scope();
+
+  print(scope.use(result));
 }
