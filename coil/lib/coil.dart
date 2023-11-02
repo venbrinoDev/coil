da577f1a4f1d3ca663cf42f258de9c321c3c67a4
Jeremiah Ogbomo <jeremiahogbomo@gmail.com>
2023-11-02 08:39:01 +0100

    Fresh

--- coil/lib/coil.dart
+++ coil/lib/coil.dart
@@ -1,1 +1,142 @@
-class Coil<T> {}
+import 'package:meta/meta.dart';
+
+typedef VoidCallback = void Function();
+typedef ValueCallback<T> = T Function();
+typedef CoilFactory<T> = T Function(Ref<T> ref);
+typedef CoilMutation<T> = T Function(T value);
+typedef CoilListener<T> = void Function(T? previous, T next);
+typedef CoilSubscription<T> = ({ValueCallback<T> get, VoidCallback dispose});
+typedef CoilFamily<U, V extends Coil> = V Function(U arg);
+typedef CoilFamilyFactory<T, U> = T Function(Ref ref, U arg);
+
+@optionalTypeArgs
+abstract class Ref<U> {
+  T get<T>(
+    Coil<T> coil, {
+    bool listen = true,
+  });
+
+  T mutate<T>(Coil<T> coil, CoilMutation<T> updater);
+
+  U mutateSelf(CoilMutation<U?> updater);
+
+  CoilSubscription<T> listen<T>(
+    Coil<T> coil,
+    CoilListener<T> listener, {
+    bool fireImmediately = false,
+  });
+
+  void invalidate<T>(Coil<T> coil);
+
+  void onDispose(VoidCallback callback);
+}
+
+@optionalTypeArgs
+base class Coil<T> {
+  Coil(this.factory, {this.key, this.debugName});
+
+  static CoilFamily<U, Coil<T>> family<T, U>(
+    CoilFamilyFactory<T, U> factory, {
+    String? debugName,
+  }) {
+    return (U arg) => Coil((ref) => factory(ref, arg), key: arg, debugName: debugName);
+  }
+
+  @internal
+  final CoilFactory<T> factory;
+
+  @internal
+  final Object? key;
+
+  @internal
+  final String? debugName;
+
+  CoilElement<T> createElement() => CoilElement<T>(this);
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) || other is Coil && runtimeType == other.runtimeType && key == other.key;
+
+  @override
+  int get hashCode => key?.hashCode ?? identityHashCode(this);
+
+  @override
+  String toString() => 'Coil($debugName)[$hashCode]';
+}
+
+@optionalTypeArgs
+class Scope<U> implements Ref<U> {
+  Scope()
+      : _owner = null,
+        _elements = {};
+
+  final CoilElement<U>? _owner;
+  final Map<Coil, CoilElement> _elements;
+
+  @override
+  T get<T>(Coil<T> coil, {bool listen = true}) {
+    throw UnimplementedError();
+  }
+
+  @override
+  T mutate<T>(Coil<T> coil, CoilMutation<T> updater) {
+    throw UnimplementedError();
+  }
+
+  @override
+  @protected
+  @Deprecated('???')
+  U mutateSelf(CoilMutation<U?> updater) {
+    throw UnimplementedError();
+  }
+
+  @override
+  CoilSubscription<T> listen<T>(
+    Coil<T> coil,
+    CoilListener<T> listener, {
+    bool fireImmediately = false,
+  }) {
+    throw UnimplementedError();
+  }
+
+  @override
+  void invalidate<T>(Coil<T> coil) {
+    throw UnimplementedError();
+  }
+
+  @override
+  void onDispose(VoidCallback callback) {
+    // TODO: Unimplemented
+  }
+
+  void dispose() {
+    // TODO: Unimplemented
+  }
+}
+
+@optionalTypeArgs
+class CoilElement<T> {
+  CoilElement(this._coil);
+
+  final Coil<T> _coil;
+
+  T get state {
+    assert(_state != null, 'Needs to have its state set at least once');
+    return _state!;
+  }
+
+  T? _state;
+
+  set state(T value) {
+    _state = value;
+  }
+
+  @override
+  String toString() {
+    if (_coil.debugName case final debugName?) {
+      return 'CoilElement($debugName)';
+    }
+
+    return runtimeType.toString();
+  }
+}
--- coil_example/bin/coil_example.dart
+++ coil_example/bin/coil_example.dart
@@ -1,3 +1,88 @@
-void main() {
-  print('Hello world');
+import 'dart:async';
+
+import 'package:coil/coil.dart';
+
+final firstname = Coil((_) => 'First', debugName: 'firstname');
+final lastname = Coil((_) => 'Last', debugName: 'lastname');
+final age = Coil((_) => 0, debugName: 'age');
+
+final doubleAge = Coil((ref) => ref.get(age) * 2, debugName: 'double-age');
+final result = Coil((ref) => '${ref.get(firstname)} ${ref.get(lastname)} (${ref.get(age)})', debugName: 'result');
+
+final passThrough = Coil.family((_, int value) => value, debugName: 'pass-through');
+
+final counter = Coil((Ref<int> ref) {
+  final timer = Timer.periodic(Duration(milliseconds: 100), (_) {
+    ref.mutateSelf((value) => (value ?? 0) + 1);
+  });
+
+  ref.onDispose(() {
+    timer.cancel();
+  });
+
+  return 0;
+}, debugName: 'timer');
+
+void main() async {
+  final scope = Scope();
+
+  scope.onDispose(() {
+    log('on-dispose-scope', 0);
+  });
+
+  /** Primitives **/
+
+  log('result', scope.get(result));
+
+  scope.listen(age, (previous, next) {
+    log('listen-age', (previous, next));
+  });
+
+  scope.listen(doubleAge, (previous, next) {
+    log('listen-double-age', (previous, next));
+  });
+
+  scope.mutate(age, (value) => value + 1);
+
+  log('result', scope.get(result));
+
+  scope.invalidate(age);
+
+  log('result', scope.get(result));
+
+  scope.mutate(age, (value) => value + 2);
+
+  log('result', scope.get(result));
+
+  scope.mutate(lastname, (value) => 'v. $value');
+
+  log('double-age', scope.get(doubleAge));
+
+  log('result', scope.get(result));
+
+  /** End Primitives **/
+
+  /** Families **/
+
+  log('pass-through', scope.get(passThrough(1)));
+
+  scope.mutate(passThrough(1), (value) => value + 1);
+
+  log('pass-through', scope.get(passThrough(1)));
+
+  /** End Families **/
+
+  /** Self Mutation **/
+
+  scope.listen(counter, (previous, next) {
+    log('listen-counter', (previous, next));
+  });
+
+  /** End Self Mutation **/
+
+  scope.dispose();
+}
+
+void log<T>(String tag, T object) {
+  print((tag, object).toString());
 }
